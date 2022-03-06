806065bd019c36d0f579408d6280dd07b216a7c8
xxxx-jeung <bioman3238@gmail.com>
2022-03-06 15:50:28 +0900

    init

---
+++ src/main/java/didden/diddenspring/DiddenSpringApplication.java
@@ -0,0 +1,13 @@
+package didden.diddenspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiddenSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiddenSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/didden/diddenspring/DiddenSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package didden.diddenspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiddenSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
